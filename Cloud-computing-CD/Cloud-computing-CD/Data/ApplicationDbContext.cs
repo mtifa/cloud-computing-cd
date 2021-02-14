867ca7621dca56ddc4cb339d652fa48dfaf93168
mijo.lucic <mijo.lucic@orbicode.com>
2021-02-14 12:49:38 +0100

    Init app

---
+++ Cloud-computing-CD/Cloud-computing-CD/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cloud_computing_CD.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
